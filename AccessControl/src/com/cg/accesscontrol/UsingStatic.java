41516b70287808fb65dbd8254b214b0fd93d294c
yashugowda29 <yashuvk97@gmail.com>
2022-08-25 15:18:07 +0530

    first commit

--- AccessControl/src/com/cg/accesscontrol/UsingStatic.java
+++ AccessControl/src/com/cg/accesscontrol/UsingStatic.java
@@ -5,6 +5,13 @@ public class UsingStatic {
 	public UsingStatic(){
 		System.out.println("Using static Constructor");
 	}
+	int varinstance; // non-static // Instance Variable
+	static int varstatic; // static
+	
+	public void methodX() {
+		varinstance=4;
+		varstatic=5;
+	}
 	public static void main(String[] args) {
 		
 		
