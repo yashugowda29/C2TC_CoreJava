07530facce67bbe204afe43f2c58c2c5ee2c7d55
yashugowda29 <yashuvk97@gmail.com>
2022-08-25 15:28:36 +0530

    first commit

--- AccessControl/src/com/cg/accesscontrol/UsingStatic.java
+++ AccessControl/src/com/cg/accesscontrol/UsingStatic.java
@@ -3,7 +3,7 @@ package com.cg.accesscontrol;
 public class UsingStatic {
 	
 	public UsingStatic(){
-		System.out.println("Using static Constructor");
+		System.out.println("Using Static Constructor");
 	}
 	int varinstance; // non-static // Instance Variable
 	static int varstatic; // static
@@ -13,8 +13,24 @@ public class UsingStatic {
 		varstatic=5;
 	}
 	public static void main(String[] args) {
+		varstatic=5;
+		UsingStatic obj = new UsingStatic();
+		obj.varinstance=4;
+		System.out.println("The Instance Variable is : " + obj.varinstance);
+		System.out.println("The Static variable is : " + varstatic);
 		
+		obj.varinstance++; // 5
+		obj.varstatic++; // 6
+		System.out.println("After Incrementation");
+		System.out.println("The Instance Variable is : " + obj.varinstance);
+		System.out.println("The Static variable is : " + varstatic);
 		
+		UsingStatic obj1 = new UsingStatic();
+		obj1.varinstance++; // 1
+		obj1.varstatic++; // 7
+		System.out.println("Incrementation for Second Object");
+		System.out.println("The Instance Variable is : " + obj1.varinstance);
+		System.out.println("The Static variable is : " + obj1.varstatic);
 	}
 
 }
