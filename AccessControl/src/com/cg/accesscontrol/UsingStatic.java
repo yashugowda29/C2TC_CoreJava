eed5ce50d865c5544853d0012c73c21720f12290
yashugowda29 <yashuvk97@gmail.com>
2022-08-25 15:15:12 +0530

    first commit

---
+++ AccessControl/src/com/cg/accesscontrol/UsingStatic.java
@@ -0,0 +1,13 @@
+package com.cg.accesscontrol;
+
+public class UsingStatic {
+	
+	public UsingStatic(){
+		System.out.println("Using static Constructor");
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
