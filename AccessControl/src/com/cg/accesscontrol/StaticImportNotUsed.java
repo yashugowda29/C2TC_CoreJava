b494ddc79452bb366a0e4c328cdf768d8a10034a
yashugowda29 <yashuvk97@gmail.com>
2022-08-25 16:11:09 +0530

    first commit

---
+++ AccessControl/src/com/cg/accesscontrol/StaticImportNotUsed.java
@@ -0,0 +1,13 @@
+package com.cg.accesscontrol;
+
+import static java.lang.Math.*;
+
+public class StaticImportNotUsed {
+
+	public static void main(String[] args) {
+		
+		System.out.println(sqrt(27));
+		System.out.println(random());
+	}
+
+}
