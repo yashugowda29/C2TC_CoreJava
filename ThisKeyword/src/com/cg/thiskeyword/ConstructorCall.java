2162ee870776f355fe8282048526c9d1cefb2b66
yashugowda29 <yashuvk97@gmail.com>
2022-08-29 15:16:55 +0530

    first commit

--- ThisKeyword/src/com/cg/thiskeyword/ConstructorCall.java
+++ ThisKeyword/src/com/cg/thiskeyword/ConstructorCall.java
@@ -3,13 +3,18 @@ package com.cg.thiskeyword;
 class Testing {
 	int data=10;
 	Testing() {
-		Training t2 = new Training();
+		Training t2 = new Training(this);
 		System.out.println("In testing constructor");
+		t2.display();
 	}
 }
 class Training {
+	Testing obj;
+	Training(Testing a){
+		this.obj=a;
+	}
 	void display() {
-		System.out.println(data);
+		System.out.println(obj.data);
 	}
 }
 public class ConstructorCall {
