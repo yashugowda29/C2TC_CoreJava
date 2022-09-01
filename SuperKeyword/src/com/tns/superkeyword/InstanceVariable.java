47aa5abee34208173b8ff1b7e42ebfbf89240929
yashugowda29 <yashuvk97@gmail.com>
2022-09-01 16:24:51 +0530

    first commit

---
+++ SuperKeyword/src/com/tns/superkeyword/InstanceVariable.java
@@ -0,0 +1,23 @@
+package com.tns.superkeyword;
+
+class Animal {
+	String color="Brown";
+}
+class Dog extends Animal {
+	String color="White";
+	void printcolor() {
+		System.out.println(color); // White
+		System.out.println(super.color); // Brown
+	}
+}
+public class InstanceVariable {
+
+	public static void main(String[] args) {
+		
+		Dog d = new Dog();
+		System.out.println(d.color); // White
+		d.printcolor();
+		
+	}
+
+}
